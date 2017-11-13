646ff0267f1481d9794e7eaa7495c78b10402812
Dušan Simić <dusan.simic1810@gmail.com>
2017-11-13 18:19:16 +0100

    Dodat domaci - MongoDB Profesori

---
+++ mongodb-profesor/AddProfesor.js
@@ -0,0 +1,43 @@
+const MongoClient = require('mongodb').MongoClient;
+const Profesori = [
+	require('./profesor1.json'),
+	require('./profesor2.json'),
+	require('./profesor3.json')
+];
+const Komentari = [
+	require('./komentari1.json'),
+	require('./komentari2.json'),
+	require('./komentari3.json')
+];
+
+MongoClient.connect('mongodb://localhost:27017/profesori', (err, db) => {
+	if (err) {
+		throw err;
+	}
+
+	const colletionProfesori = db.collection('profesori');
+
+	colletionProfesori.insertMany(Profesori, (err, res) => {
+		db.close();
+		console.log('Profesori:');
+
+		if (err) {
+			throw err;
+		}
+
+		console.log('Inserted count: ' + res.insertedCount);
+	});
+
+	const collectionKomentari = db.collection('komentari');
+
+	collectionKomentari.insertMany(Komentari, (err, res) => {
+		db.close();
+		console.log('Komentari:');
+
+		if (err) {
+			throw err;
+		}
+
+		console.log('Inserted count: ' + res.insertedCount);
+	});
+});
